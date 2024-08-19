f7a32015e08192b6a6019aaa72fcb637edbdd1b9
vurtne <330256579@qq.com>
2024-08-19 16:40:28 +0800

    add ollamallM

--- src/llm.py
+++ src/llm.py
@@ -1,6 +1,7 @@
 import os
 from openai import OpenAI  # 导入OpenAI库用于访问GPT模型
 from logger import LOG  # 导入日志模块
+from ollama import Client as OllamaClient
 
 class LLM:
     def __init__(self):
@@ -35,6 +36,50 @@ class LLM:
             LOG.debug("GPT response: {}", response)
             # 返回模型生成的内容
             return response.choices[0].message.content
+        except Exception as e:
+            # 如果在请求过程中出现异常，记录错误并抛出
+            LOG.error("An error occurred while generating the report: {}", e)
+            raise
+
+
+# from logger import LOG  # 导入日志模块（演示时直接导入）
+
+class OllamaLLM:
+    def __init__(self, model="qwen2:7b"):
+        # 创建一个OpenAI客户端实例
+        self.client = OllamaClient(host="http://117.50.216.153:11434")
+        # 确定使用的模型版本
+        self.model = model
+        # 配置日志文件，当文件大小达到1MB时自动轮转，日志级别为DEBUG
+        LOG.add("daily_progress/llm_logs.log", rotation="1 MB", level="DEBUG")
+
+    def generate_daily_report(self, markdown_content, dry_run=False):
+        # 构建一个用于生成报告的提示文本，要求生成的报告包含新增功能、主要改进和问题修复
+        prompt = f"以下是项目的最新进展，根据功能合并同类项，形成一份简报，至少包含：1）新增功能；2）主要改进；3）修复问题；:\n\n{markdown_content}"
+        
+        if dry_run:
+            # 如果启用了dry_run模式，将不会调用模型，而是将提示信息保存到文件中
+            LOG.info("Dry run mode enabled. Saving prompt to file.")
+            with open("daily_progress/prompt.txt", "w+") as f:
+                f.write(prompt)
+            LOG.debug("Prompt saved to daily_progress/prompt.txt")
+            return "DRY RUN"
+
+        # 日志记录开始生成报告
+        LOG.info(f"Starting report generation using model: {self.model}.")
+        
+        try:
+            # 调用OpenAI GPT模型生成报告
+            response = self.client.chat(
+                model=self.model,  # 指定使用的模型版本
+                messages=[
+                    {"role":"system","content":"你是一个生成github项目简报的专家，根据用户的需求生成简报,请使用中文"},
+                    {"role": "user", "content": prompt}  # 提交用户角色的消息
+                ]
+            )
+            LOG.debug(f"{self.model} response: {response}")
+            # 返回模型生成的内容
+            return response["message"]["content"]
         except Exception as e:
             # 如果在请求过程中出现异常，记录错误并抛出
             LOG.error("An error occurred while generating the report: {}", e)
